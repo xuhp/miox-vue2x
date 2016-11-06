e3e45bb6c9bd5283778d86d448cb002a434e53a7
evio <shenyunjie@51zhangdan.com.cn>
2016-11-06 21:48:03 +0800

    delete unused code

--- src/util.js
+++ src/util.js
@@ -2,45 +2,6 @@
  * Created by evio on 16/10/26.
  */
 
-import isClass from 'is-class';
-import Vue from 'vue';
-
-const toString = Object.prototype.toString;
-
-export const each = function each(objects, cb){
-    const keys = Object.keys(objects);
-    let count = keys.length;
-
-    while ( count-- ) {
-        cb(objects[keys[count]], keys[count]);
-    }
-};
-
-export const compile = function compile(Value){
-    if ( isComponent(Value) ){
-        const result = new Value();
-
-        if ( result.toJSON ){
-            return result.toJSON();
-        }
-    }
-    return Value;
-};
-
-export const isFunction = function(fn){
-    return typeof fn === 'function';
-};
-
-export const isComponent = function(Value){
-    return isClass(Value) || isFunction(Value);
-};
-
-export const isVueObject = function(Value){
-    return toString.call(Value) === '[object Object]';
-};
-
-export const isObject = isVueObject;
-
 export const makeDirectiveUrlParams = function(method, ctx){
     return {
         bind(el, binding, vnode){
